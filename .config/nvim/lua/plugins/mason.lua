920bcfe6d918f82ac8409560b70968017205adf7
Ben Poppy <me@benpoppy.dev>
2026-01-12 22:18:46 +0000

    refactor(nvim): remove mason workaround

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,12 +1,12 @@
-return {
-  "williamboman/mason.nvim",
-  opts = function(_, opts)
-    vim.list_extend(opts.ensure_installed, {
-      "gofumpt",
-      "goimports",
-      "gopls",
-      "ruff",
-      "templ",
-    })
-  end,
-}
+ return {
+   "mason-org/mason.nvim",
+   opts = function(_, opts)
+     vim.list_extend(opts.ensure_installed, {
+       "gofumpt",
+       "goimports",
+       "gopls",
+       "ruff",
+       "templ",
+     })
+   end,
+ }
